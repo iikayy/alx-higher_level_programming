b9337051d2247612c95672ea9525bdcbddf19fa4
iikayy <ii_kayy@yahoo.com>
2023-08-16 02:01:14 -0700

    import and modules

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def add(a,b):
-    a=1
-    b=2
-    sum=a+b
-    print(a,'+', b,'=',sum)
+def add(a, b):
+    a = 1
+    b = 2
+    sum = a + b
+    print(a, '+', b, '=', sum)
--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-def add(a,b):
-    return(a,'+', b,'=', a + b)
+def add(a, b):
+    return(a, '+', b, '=', a + b)
 
-def sub(a,b):
-    return(a,'-', b,'=', a - b)
 
-def mul(a,b):
-    return(a,'*', b,'=', a * b)
+def sub(a, b):
+    return(a, '-', b, '=', a - b)
 
-def div(a,b):
-    return(a,'/', b,'=', a / b)
+
+def mul(a, b):
+    return(a, '*', b, '=', a * b)
+
+
+def div(a, b):
+    return(a, '/', b, '=', a / b)
